555055031e6ea23e20d646c2c0ba107f96dc0498
shucheng <shucheng@bd-apaas.com>
2020-12-29 19:39:10 +0800

    v1.28.1 fix bug

--- .beagle/build.sh
+++ .beagle/build.sh
@@ -6,16 +6,16 @@ mkdir -p dist
 
 export GOARCH=amd64
 make build
-mv /go/bin/kiali dist/kiali-$GOARCH
+mv /go/bin/kiali dist/kiali-linux-$GOARCH
 
 export GOARCH=arm64
 make build
-mv /go/bin/kiali dist/kiali-$GOARCH
+mv /go/bin/kiali dist/kiali-linux-$GOARCH
 
 export GOARCH=ppc64le
 make build
-mv /go/bin/kiali dist/kiali-$GOARCH
+mv /go/bin/kiali dist/kiali-linux-$GOARCH
 
 export GOARCH=mips64le
 make build
-mv /go/bin/kiali dist/kiali-$GOARCH
+mv /go/bin/kiali dist/kiali-linux-$GOARCH
